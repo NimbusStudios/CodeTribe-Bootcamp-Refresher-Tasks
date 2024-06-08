6860da430c1b896f959ba76961d9857e5806d54d
NimbusStudios <165320227+NimbusStudios@users.noreply.github.com>
2024-06-08 17:15:45 +0200

    Add files via upload
    Task 1

---
+++ variable reassignment.js
@@ -0,0 +1,5 @@
+let favoriteColour = "Blue";
+console.log("Favorite Colour:", favoriteColour);
+
+favoriteColour = "Red";
+console.log("New Favorite Colour:", favoriteColour);
---
+++ variables.js
@@ -0,0 +1,11 @@
+
+
+let name = "Nkululeko Sedibe";
+let age = "34";
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Is Student:", isStudent);
+console.log("Age:", age);
+
+
